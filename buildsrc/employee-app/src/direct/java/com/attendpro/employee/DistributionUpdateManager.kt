a9864ae9cf6fe69dd10003d60d410d4e70e50dbc
nmg200101-collab <nmg200101@gmail.com>
2026-09-11 11:08:44 +0300

    RC2: separate Play and Direct update channels

---
+++ buildsrc/employee-app/src/direct/java/com/attendpro/employee/DistributionUpdateManager.kt
@@ -0,0 +1,14 @@
+package com.attendpro.employee
+
+import android.app.Activity
+import com.attendpro.core.AppUpdateManager
+
+object DistributionUpdateManager {
+    const val DEFAULT_SERVER = AppUpdateManager.DEFAULT_SERVER
+
+    fun check(activity: Activity, serverUrl: String, channel: String, manual: Boolean = false) {
+        AppUpdateManager.check(activity, serverUrl, channel, manual)
+    }
+
+    fun resume(activity: Activity) = Unit
+}
