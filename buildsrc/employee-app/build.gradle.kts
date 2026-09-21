f57dd7892a4c598e00873b871fb1fa2a58750f48
nmg200101-collab <nmg200101@gmail.com>
2026-09-21 21:38:14 +0300

    Bump ATTEND-PRO to V133 resilient DNS

--- buildsrc/employee-app/build.gradle.kts
+++ buildsrc/employee-app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.attendpro.employee"
         minSdk = 26
         targetSdk = 36
-        versionCode = 132
-        versionName = "2.0.0-RC29-SMART-RESTORE1"
+        versionCode = 133
+        versionName = "2.0.0-RC29-RESILIENT-DNS1"
     }
 
     compileOptions {
