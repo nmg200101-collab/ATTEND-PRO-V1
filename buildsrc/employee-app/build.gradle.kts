69d4d2bf3b7f63518b83466d5e58dbc864c3acdf
nmg200101-collab <nmg200101@gmail.com>
2026-09-21 22:42:47 +0300

    Bump ATTEND-PRO to V134 DNS recovery

--- buildsrc/employee-app/build.gradle.kts
+++ buildsrc/employee-app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.attendpro.employee"
         minSdk = 26
         targetSdk = 36
-        versionCode = 133
-        versionName = "2.0.0-RC29-RESILIENT-DNS1"
+        versionCode = 134
+        versionName = "2.0.0-RC29-DNS-RECOVERY2"
     }
 
     compileOptions {
