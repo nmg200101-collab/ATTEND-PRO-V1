1826029422e0ece4464219dce50c85c42656af03
nmg200101-collab <nmg200101@gmail.com>
2026-09-22 21:21:50 +0300

    Start V142 fast employee message polling

--- buildsrc/employee-app/src/main/java/com/attendpro/employee/MainActivity.kt
+++ buildsrc/employee-app/src/main/java/com/attendpro/employee/MainActivity.kt
@@ -100,6 +100,7 @@ class MainActivity : Activity() {
         if (!enforceOfficialBuild1982()) return
         identity = EmployeeIdentityStore(this)
         EmployeeMessageFastPoller142.start(this)
+        EmployeeMessageFastPoller142.start(this)
         val rc7Migration = getSharedPreferences("attend_rc7_migration", MODE_PRIVATE)
         if (identity.isConfigured && !rc7Migration.getBoolean("presence_enabled_once", false)) {
             identity.autoPresence = true
