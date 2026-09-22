3b31136b03fc16f6673bd7c87e9ce53350f6955e
nmg200101-collab <nmg200101@gmail.com>
2026-09-22 12:25:43 +0300

    Bump ATTEND-PRO to V137 Receiver Stabilization

--- buildsrc/employee-app/build.gradle.kts
+++ buildsrc/employee-app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.attendpro.employee"
         minSdk = 26
         targetSdk = 36
-        versionCode = 136
-        versionName = "2.0.0-RC29-IPV4-RECEIVER-EMPLOYEE1"
+        versionCode = 137
+        versionName = "2.0.0-RC29-V137-RECEIVER-STABILIZATION"
     }
 
     compileOptions {
